0419b7e239e31e9a5f880aa8e3ad9f7107723c9b
Elliot Wright <wright.elliot@gmail.com>
2016-08-22 00:03:00 +0100

    Added locks, and a little bit of additional separative commentary.

--- url/gateway.go
+++ url/gateway.go
@@ -1,10 +1,16 @@
 package url
 
+import "sync"
+
+// -- URL Gateway Interface
+
 // Gateway is the interface for find URL information in some data store.
 type Gateway interface {
 	FindByReference(string) (*URL, error)
 	FindLatest() (*URL, error)
 }
+
+// -- Mock URL Gateway
 
 // MockGateway provides an implementation of the Gateway interface that allows pre-settings values,
 // for use in tests.
@@ -22,15 +28,25 @@ func (g MockGateway) FindLatest() (*URL, error) {
 	return g.mockURL
 }
 
+// -- MySQL URL Gateway
+
 // MysqlGateway provides an implementation of the Gateway interface that uses Mysql as a data store.
-type MysqlGateway struct{}
+type MysqlGateway struct {
+	sync.RWMutex
+}
 
 // FindByReference finds a URL by it's reference.
 func (g MysqlGateway) FindByReference(reference string) (*URL, error) {
+	g.RLock()
+	defer g.RUnlock()
+
 	return &URL{}
 }
 
 // FindLatest finds the latest URL in the data store.
 func (g MysqlGateway) FindLatest() (*URL, error) {
+	g.RLock()
+	defer g.RUnlock()
+
 	return &URL{}
 }
